7daedee87afa8a6f2786b268bacec0dd4ca8be68
mani@1901 <mani1992004@gmail.com>
2025-09-07 16:28:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
+from datetime import datetime
 
 # ================= Login Page =================
 def login():
